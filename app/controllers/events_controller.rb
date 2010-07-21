a2763efc93b5b459db9fc2211ac7129307857d45
Prateek Dayal <prateek@muziboo.com>
2010-07-21 16:53:22 +0530

    basic event creation stuff is in place

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,13 @@ class EventsController < ApplicationController
 
   end
 
+  def new
+    start = Time.parse(params[:date].gsub(/GMT.*$/,''))
+    @event = Event.new(:start => start)
+    respond_to do |format|
+      format.html { render :nothing => true }
+      format.js
+    end
+  end
+
 end
