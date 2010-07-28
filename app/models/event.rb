95f0ec31219a27768d6a866b901b9ba9dd9740ce
Prateek Dayal <prateek@muziboo.com>
2010-07-28 13:01:12 +0530

    phone numbers can be deleted

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,8 @@ class Event < ActiveRecord::Base
   has_many  :phone_numbers, :dependent => :destroy
   
 
-  accepts_nested_attributes_for :phone_numbers
+  accepts_nested_attributes_for :phone_numbers, 
+      :reject_if => lambda { |a| a[:number].blank? }, 
+      :allow_destroy => true 
 
 end
