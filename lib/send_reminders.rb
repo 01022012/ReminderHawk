c38fbf9b638bc6005549a4732a3c1f7f09c514df
Prateek Dayal <prateek@muziboo.com>
2010-07-29 11:56:30 +0530

    script to send sms reminders now complete

--- lib/send_reminders.rb
+++ lib/send_reminders.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'mysql'
 require 'yaml'
+require 'net/http'
+require 'uri'
+require 'cgi'
 
 ENV['RAILS_ENV'] ||= "development"
 
@@ -23,7 +26,28 @@ rescue Mysql::Error => e
   puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
 end
 
-reminders = con.query("SELECT remind_at, event_id from reminders")
+reminders = con.query("SELECT id, event_id, remind_at  FROM `reminders` WHERE (remind_at < '#{Time.now}' and sent is false)")
 
-reminders.each {|r| puts r}
+reminders.each do |r|
+  puts "#{r[0]} - #{r[1]} - #{r[2]}"
+  event = con.query("SELECT id, subject from `events` WHERE (`events`.`id` = #{r[1]})").fetch_row
+  message = event[1]
+
+  puts message
+
+  phone_number_set = con.query("SELECT number FROM `phone_numbers` WHERE (`phone_numbers`.event_id = #{event[0]})")
+
+  phone_numbers = []
+  
+  phone_number_set.each {|p| phone_numbers << p[0]}
+
+  puts phone_numbers
+
+  # Time to send the reminder
+#  Net::HTTP.get_print URI.parse("http://bulksms.gateway4sms.com/pushsms.php?username=reminderhawk&password=790188&to=#{phone_numbers.join(',')}&message=#{CGI::escape(message)}")
+
+  # Mark as sent
+  con.query("UPDATE `reminders` SET `updated_at` = '#{Time.now}', `sent` = 1 WHERE `id` = #{r[0]}")
+  
+end
 
