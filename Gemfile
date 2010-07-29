117db78888a3efc3fd4870b826a4ea979cbd1654
Prateek Dayal <prateek@muziboo.com>
2010-07-29 19:14:30 +0530

    adding Gemfile

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source :gemcutter
+gem 'rails', '2.3.2'
+gem 'mysql'
+gem 'subdomain-fu'
+
+
+group :cucumber do 
+  gem 'cucumber-rails'
+  gem 'database_cleaner'
+  gem 'webrat'
+  gem 'rspec'
+  gem 'rspec-rails'
+  gem 'launchy'
+  gem 'pickle'
+  gem 'factory_girl', '1.2.4'
+  gem 'capybara'
+  gem 'mocha'
+  gem 'spork'
+  gem 'ZenTest'
+  gem 'autotest-rails'
+  gem 'autotest-growl'
+end
--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :deploy_via, :remote_cache
 set :use_sudo,  false
 
 # Hooks to do specific stuff
-after "deploy:update_code", "reminderhawk:config"
+after "deploy:update_code", "reminderhawk:config", "bundler:bundle_new_release"
 after "deploy", "deploy:cleanup"
 
 namespace(:reminderhawk) do
@@ -37,3 +37,25 @@ namespace(:reminderhawk) do
     CMD
   end
 end
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+  
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} && bundle install --without cucumber"
+  end
+  
+  task :lock, :roles => :app do
+    run "cd #{current_release} && bundle lock;"
+  end
+  
+  task :unlock, :roles => :app do
+    run "cd #{current_release} && bundle unlock;"
+  end
+end
+
