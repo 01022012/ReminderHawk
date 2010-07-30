abd574bc34b16d72b459be7606fea412146332cc
Prateek Dayal <prateek@muziboo.com>
2010-07-30 06:48:38 +0530

    some more validations and cleaning up the email validation for users

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -2,5 +2,6 @@ class PhoneNumber < ActiveRecord::Base
 
   belongs_to  :event
 
+  validates_format_of :number, :with => /\d{10}/, :message => "Please enter a 10 digit Indian Mobile number"
 
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -11,4 +11,10 @@ class Reminder < ActiveRecord::Base
     self.remind_at = event.start - duration.send(INTERVAL[unit])
   end
 
+  def validate
+    RAILS_DEFAULT_LOGGER.info("Event is #{event_id}")
+    errors.add_to_base "Reminder has to be atleast 10 minutes from now" if  (Time.now - duration.send(INTERVAL[unit])) > (Time.now - 10.minutes)
+    errors.add_to_base "Reminder has to be atleast 10 minutes from now" if  (Time.now - duration.send(INTERVAL[unit])) > (Time.now - 10.minutes)
+  end
+
 end
