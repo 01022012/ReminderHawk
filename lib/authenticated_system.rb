b4ba22175c4e442955595e1edd486c733c9756a0
Prateek Dayal <prateek@muziboo.com>
2010-07-30 23:41:54 +0530

    added login message

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -64,6 +64,7 @@ module AuthenticatedSystem
     def access_denied
       respond_to do |format|
         format.html do
+          flash[:notice] = "Please login to continue"
           store_location
           redirect_to new_session_path
         end
