3d1b7dfa0a73d00893ba29dd916836cb01ea7b29
Prateek Dayal <prateek@muziboo.com>
2010-07-30 06:33:18 +0530

    event has validations now

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,8 @@ class Event < ActiveRecord::Base
   validates_presence_of :title, :subject, :start, :end
   has_many  :phone_numbers, :dependent => :destroy
   has_many  :reminders, :dependent => :destroy
+
+  validates_length_of :subject, :in => 4..140
   
 
   accepts_nested_attributes_for :phone_numbers, 
@@ -13,4 +15,9 @@ class Event < ActiveRecord::Base
 
   accepts_nested_attributes_for :reminders, 
       :allow_destroy => true 
+
+  def validate
+    errors.add_to_base "The event cannot end before it starts" if self.end < start
+    errors.add_to_base "Event has to start atleast one hour from now" if start < (Time.now + 1.hour)
+  end
 end
